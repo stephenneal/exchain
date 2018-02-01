7cae90cb0c5927de43388408c84e133c2a0cf7fb
Stephen Neal <Stephen.Neal2@sa.gov.au>
2018-02-01 15:08:25 +1030

    Add access control headers

--- microservice/startup.go
+++ microservice/startup.go
@@ -2,8 +2,11 @@ package microservice
 
 import (
 	"net/http"
+	"fmt"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 
     "github.com/patrickmn/go-cache"
@@ -51,12 +54,41 @@ func Start(port string) {
 		httptransport.EncodeJSONResponse,
 	)
 
-	http.Handle("/pub/v1/getTicker", getTickerHandler)
-	http.Handle("/pub/v1/getTickers", getTickersHandler)
+	mux := http.NewServeMux()
+
+	mux.Handle("/pub/v1/getTicker", getTickerHandler)
+	mux.Handle("/pub/v1/getTickers", getTickersHandler)
+
+	http.Handle("/", accessControl(mux))
 	http.Handle("/metrics", promhttp.Handler())
-	logger.Log("msg", "HTTP", "addr", port)
+
 	if !strings.HasPrefix(":", port) {
 		port = ":" + port
 	}
-	logger.Log("err", http.ListenAndServe(port, nil))
+	errs := make(chan error, 2)
+	go func() {
+		logger.Log("transport", "http", "address", port, "msg", "listening")
+		errs <- http.ListenAndServe(port, nil)
+	}()
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	logger.Log("terminated", <-errs)
+}
+
+func accessControl(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
 }
